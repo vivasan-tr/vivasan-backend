658bafcdc538d6bfedf4cfe9b10cfee844173944
vivasan-tr <246000299+vivasan-tr@users.noreply.github.com>
2025-11-25 05:09:33 -0800

    Minio implement

--- medusa-config.ts
+++ medusa-config.ts
@@ -20,6 +20,31 @@ module.exports = defineConfig({
     backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
     modules: [
+          {
+      key: Modules.FILE,
+      resolve: '@medusajs/file',
+      options: {
+        providers: [
+          ...(MINIO_ENDPOINT && MINIO_ACCESS_KEY && MINIO_SECRET_KEY ? [{
+            resolve: './src/modules/minio-file',
+            id: 'minio',
+            options: {
+              endPoint: MINIO_ENDPOINT,
+              accessKey: MINIO_ACCESS_KEY,
+              secretKey: MINIO_SECRET_KEY,
+              bucket: MINIO_BUCKET // Optional, default: medusa-media
+            }
+          }] : [{
+            resolve: '@medusajs/file-local',
+            id: 'local',
+            options: {
+              upload_dir: 'static',
+              backend_url: `${BACKEND_URL}/static`
+            }
+          }])
+        ]
+      }
+    },
     {
       resolve: "@medusajs/medusa/cache-redis",
       options: {
